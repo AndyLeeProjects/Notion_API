bf3096cbe27cba6d6dc9ed67f4d4d566313ca0a5
AndyLeeProjects <anddy0622@gmail.com>
2023-12-30 16:24:41 -0500

    chore: update path logic

--- tests/test_notion_api.py
+++ tests/test_notion_api.py
@@ -8,7 +8,7 @@ from main import NotionAPI
 def notion_api():
     return NotionAPI(token_key="fake_token")
 
-@patch('retrieve.ConnectNotion')
+@patch('main.ConnectNotion')
 def test_get_database(mock_connect, notion_api):
     # Setup the mock
     mock_instance = mock_connect.return_value
@@ -18,11 +18,11 @@ def test_get_database(mock_connect, notion_api):
     result = notion_api.get_database(database_id="fake_id")
 
     # Assertions
-    mock_connect.assert_called_with(database_id="fake_id", token_key="fake_token", filters=None)
+    mock_connect.assert_called_with(database_id="fake_id", token_key="fake_token", filters=None, user_db=False)
     mock_instance.retrieve_data.assert_called_with("dataframe")
     assert result == "mocked data"
 
-@patch('update.update_notion')
+@patch('main.update_notion')
 def test_update_element_db(mock_update, notion_api):
     # Prepare test data
     content = {"some": "data"}
@@ -34,7 +34,7 @@ def test_update_element_db(mock_update, notion_api):
     # Assert that the mock was called correctly
     mock_update.assert_called_once_with(content, pageId, notion_api.headers)
 
-@patch('update.add_new_row_to_notion_database')
+@patch('main.add_new_row_to_notion_database')
 def test_add_element_db(mock_add_new, notion_api):
     # Set up test data
     content = {"other": "data"}
@@ -44,4 +44,4 @@ def test_add_element_db(mock_add_new, notion_api):
     notion_api.add_element_db(content, databaseId)
 
     # Assertions
-    mock_add_new.assert_called_with(content, databaseId, notion_api.headers)
+    mock_add_new.assert_called_once_with(content, databaseId, notion_api.headers)
