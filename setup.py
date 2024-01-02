451c28db647c03f0bab72bf06924c7832bf9e0e9
AndyLeeProjects <anddy0622@gmail.com>
2024-01-01 23:06:09 -0500

    feat: set up settings

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='notion_api_sdk',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your package dependencies here
+    ],
+    # other metadata like author, description, etc.
+)
